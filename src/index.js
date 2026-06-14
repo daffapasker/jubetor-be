c4ba30007141ce92d3eaa1345a027c630c3e0c55
daffapasker <daffapasker121@gmail.com>
2026-06-14 16:30:43 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import app from "./routes/route.js";
 
-const PORT = 3000;
-
-
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+
 import userRoutes from "./user.routes.js";
 import projectRoutes from "./project.routes.js";
 import authRoutes from "./auth.route.js";
@@ -8,8 +10,17 @@ import trackRecordRoutes from "./trackRecords.route.js";
 
 const app = express();
 
+// Middleware
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/projects", projectRoutes);
@@ -17,4 +28,12 @@ app.use("/api/articles", articleRoutes);
 app.use("/api/design-catalogs", designCatalogRoutes);
 app.use("/api/track-records", trackRecordRoutes);
 
-export default app
+// Health Check
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "API is running",
+  });
+});
+
+export default app;
